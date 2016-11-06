707930cd59eeeb60b7247e78cf4316f6cae952dd
BunnY <w@bunnywong.com>
2016-11-06 22:58:53 +0800

    update $is_admin + add jQuery UI date picker

--- wp-content/themes/Parallax-One-child/template-1.php
+++ wp-content/themes/Parallax-One-child/template-1.php
@@ -3,12 +3,16 @@
   Template Name: 1. block view(clone from blog)
   */
   $user_id = get_current_user_id(); //@TODO: admin view all
-  $user_role =  get_userdata($user_id);
-  $user_role =  $user_role->roles;
-  $user_role =  $user_role[0];
+  $is_admin =  get_userdata($user_id);
+  $is_admin =  $user_role->roles;
+  $is_admin =  $user_role[0];
+  if ($is_admin[0] === 'administrator') {
+    $is_admin = true;
+  }
+  else {
+    $is_admin = false;
+  }
 
-  get_userdata($user_id);
-  dvm($user_id);
   $posts = query_posts( array( 'author'=> $user_id, 'post_type' => 'post', 'posts_per_page' => 6, 'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1 ) ) );
   // include( get_my_block_view_template() );
   wp_reset_postdata();
@@ -21,7 +25,7 @@
     // Get meta by post ID
     $custom_field = get_post_meta($val->ID);
     $str .= '<table class="table table-hover my-table"><tbody>';
-    foreach (my_title_in_from('a', $user_role) as $k => $v) {
+    foreach (my_title_in_from('a', $is_admin) as $k => $v) {
       $str .= '<tr><td>'.($v) . '</td><td>' .my_field_render($custom_field[$v], $v) . '</td></tr>';
     }
     $str .= '</tbody></table>';
@@ -38,3 +42,8 @@
   }
 ?>
 
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+   jQuery( ".date input" ).datepicker({dateFormat: "yy-mm-dd"});
+</script>
