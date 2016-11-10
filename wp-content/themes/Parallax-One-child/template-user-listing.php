7a2d5f5fcf58da2d19ecff3c3101a9e79386fe58
BunnY <w@bunnywong.com>
2016-11-11 01:11:52 +0800

    add user list view template

---
+++ wp-content/themes/Parallax-One-child/template-user-listing.php
@@ -0,0 +1,54 @@
+<?php
+  /*
+  Template Name: User Listing
+  */
+
+  include( get_my_block_view_template() );
+  wp_reset_postdata();
+  get_header();
+
+  $users =  get_users( $args );
+
+  // $all_meta_for_user = get_user_meta( 1);
+  // vd( $all_meta_for_user );
+?>
+
+<div class="container">
+  <div class="col-xs-12">
+    <h1 class="text-center">Admin Home</h1>
+  </div>
+
+  <div class="col-xs-12">
+    <p>
+
+    </p>
+  </div>
+  <table class="table table-responsive table-striped table-bordered table-hover my-table">
+    <thead>
+      <th>User Name</th>
+      <th >Email</th>
+      <th>存入股本</th>
+      <th>股息報表</th>
+      <th>提取或轉移</th>
+    </thead>
+    <tbody>
+    <?php //vd($users); ?>
+      <?php for($i = 0; $i < count($users); $i++): ?>
+        <tr>
+          <td><a href="/user?uid=<?php echo $users[$i]->data->ID;?>"><?php echo $users[$i]->data->user_login;?></a></td>
+          <td><?php echo $users[$i]->data->user_email;?></td>
+          <td><a href="from-a?uid=<?php echo $users[$i]->data->ID;?>"><button type="button" class="btn btn-default">edit</button></a></td>
+          <td>@TODO:<a href="from-a?uid=<?php echo $users[$i]->data->ID;?>"><button type="button" class="btn btn-primary">edit</button></a></td>
+          <td><a href="from-b?uid=<?php echo $users[$i]->data->ID;?>"><button type="button" class="btn btn-success">edit</button></a></td>
+
+        </tr>
+      <?php endfor; ?>
+    </tbody>
+  </table>
+
+  <a href="/new-user"><button type="button" class="center-block btn btn-primary btn-lg">註冊新股東</button></a>
+
+  <div class="db text-center">@TODO: enhance fields option by request</div>
+  <?php //dvm($users); ?>
+  <?php //dvm($users[0]); ?>
+</div>
